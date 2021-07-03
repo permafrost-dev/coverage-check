038e692f2cb77f2c6cab4d789817829a3d513ce1
Patrick Organ <patrick@permafrost.dev>
2021-07-03 12:21:05 -0400

    add additional tests for CheckCoverageCommand

--- tests/Commands/CheckCoverageCommandTest.php
+++ tests/Commands/CheckCoverageCommandTest.php
@@ -6,6 +6,7 @@ use Permafrost\CoverageCheck\Commands\CheckCoverageCommand;
 use Permafrost\CoverageCheck\Tests\TestClasses\FakeOutput;
 use PHPUnit\Framework\TestCase;
 use Spatie\Snapshots\MatchesSnapshots;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
@@ -79,4 +80,46 @@ class CheckCoverageCommandTest extends TestCase
 
         $this->assertMatchesSnapshot($this->output->writtenData);
     }
+
+    /** @test */
+    public function it_executes_the_command_with_a_valid_filename_and_fails_when_enforces_coverage()
+    {
+        $input = $this->createInput(['filename' => __DIR__ . '/../data/coverage-clover.xml', '--require' => 99]);
+
+        $result = $this->command->execute($input, $this->output);
+
+        $this->assertMatchesSnapshot($this->output->writtenData);
+        $this->assertEquals(Command::FAILURE, $result);
+    }
+
+    /** @test */
+    public function it_passes_coverage_enforcement_and_returns_a_success_exit_code()
+    {
+        $input = $this->createInput(['filename' => __DIR__ . '/../data/coverage-clover.xml', '--require' => 10]);
+
+        $result = $this->command->execute($input, $this->output);
+
+        $this->assertEquals(Command::SUCCESS, $result);
+    }
+
+    /** @test */
+    public function it_fails_coverage_enforcement_and_returns_an_error_exit_code()
+    {
+        $input = $this->createInput(['filename' => __DIR__ . '/../data/coverage-clover.xml', '--require' => 99]);
+
+        $result = $this->command->execute($input, $this->output);
+
+        $this->assertEquals(Command::FAILURE, $result);
+    }
+
+    /** @test */
+    public function it_passes_invalid_parameters_and_returns_an_invalid_argument_error_exit_code()
+    {
+        $input = $this->createInput(['filename' => __DIR__ . '/../data/missing.xml', '--require' => 99]);
+
+        $result = $this->command->execute($input, $this->output);
+
+        $this->assertEquals(Command::INVALID, $result);
+    }
+
 }
