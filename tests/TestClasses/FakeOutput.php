11f87a7e91d6d88a2a3f2f548e2246d56d0ddca9
Patrick Organ <patrick@permafrost.dev>
2022-03-12 02:27:33 -0500

    update FakeOutput class to support both symfony 5 & 6

--- tests/TestClasses/FakeOutput.php
+++ tests/TestClasses/FakeOutput.php
@@ -3,70 +3,26 @@
 namespace Permafrost\CoverageCheck\Tests\TestClasses;
 
 use Symfony\Component\Console\Formatter\OutputFormatterInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
 
-class FakeOutput implements OutputInterface
+class FakeOutput extends Output
 {
     /** @var array|string[] */
     public $writtenData = [];
-
-    public function isDecorated()
-    {
-        // TODO: Implement isDecorated() method.
-    }
-
-    public function setDecorated(bool $decorated)
-    {
-        // TODO: Implement setDecorated() method.
-    }
 
     public function write($messages, bool $newline = false, int $options = 0)
     {
         $this->writtenData[] = $messages . ($newline ? PHP_EOL : '');
     }
 
-    public function setVerbosity(int $level)
-    {
-        // TODO: Implement setVerbosity() method.
-    }
-
-    public function setFormatter(OutputFormatterInterface $formatter)
-    {
-        // TODO: Implement setFormatter() method.
-    }
-
-    public function isVeryVerbose()
-    {
-        // TODO: Implement isVeryVerbose() method.
-    }
-
-    public function isVerbose()
-    {
-        // TODO: Implement isVerbose() method.
-    }
-
-    public function isQuiet()
-    {
-        // TODO: Implement isQuiet() method.
-    }
-
-    public function isDebug()
-    {
-        // TODO: Implement isDebug() method.
-    }
-
-    public function getVerbosity()
-    {
-        // TODO: Implement getVerbosity() method.
-    }
-
-    public function getFormatter()
-    {
-        // TODO: Implement getFormatter() method.
-    }
-
     public function writeln($messages, int $options = 0)
     {
         $this->writtenData[] = $messages;
     }
+
+    protected function doWrite(string $message, bool $newline)
+    {
+        //
+    }
+
 }
